c37c75e435bd1ce6a4523a655a338f91445b28bc
nikichaturvedi <chaturvedinikita657@gmail.com>
2024-10-06 09:57:07 +0530

    https://github.com/nikichaturvedi/WanderWay

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto:{
-        secret: "mysupersecretcode",
+        secret: process.env.SECRET,
     },
     touchAfter: 24*3600,
 });
@@ -61,7 +61,7 @@ const store = MongoStore.create({
 
 const sessionOptions = {
     store,
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUnintialized: true,
     cookie: {
