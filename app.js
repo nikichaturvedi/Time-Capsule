fe87aab92e2478f58ea9f969c3acfee5140d555f
Nikita chaturvedi <130087883+nikichaturvedi@users.noreply.github.com>
2024-10-06 15:51:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use ((req, res, next) =>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
     next();
 });
 
@@ -104,7 +104,6 @@ app.all("*", (req, res, next) =>{
 app.use((err, req, res, next) =>{
     let {statusCode=500 ,message ="Something went wrong!"} = err;
     res.status(statusCode).render("error.ejs" ,{ message });
-    // res.status(statusCode).send(message);
 });
 
 app.listen(8080 ,() =>{
