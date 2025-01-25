a6e71821de381290d4fec5dc10b9a9d23b61be62
Nikita chaturvedi <130087883+nikichaturvedi@users.noreply.github.com>
2025-01-25 22:18:21 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isOwner = async (req, res, next) => {
     let {id} = req.params;
     let listing = await Listing.findById(id);
-    if (!(listing.owner.equals(res.locals.currUser._id) || res.locals.currUser._id.toString() === '67951338986c5432455e5347')) {
+    if (!(listing.owner.equals(res.locals.currUser._id)) {
         req.flash("error", "You are not the owner ");
         return res.redirect(`/listings/${id}`);
     }
@@ -12,7 +12,7 @@ next();
 module.exports.isReviewAuthor = async (req, res, next) => {
     let {id, reviewId} = req.params;
     let review = await Review.findById(reviewId);
-    if (!(review.author.equals(res.locals.currUser._id) || res.locals.currUser._id.toString() === '67951338986c5432455e5347')) {
+    if (!(review.author.equals(res.locals.currUser._id)) {
         req.flash("error", "You are not the owner ");
         return res.redirect(`/listings/${id}`);
     }
